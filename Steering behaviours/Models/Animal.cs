446016ab495cb86942b407fa555b6b9472329922
Diana Obiedkova <missdee31102001@gmail.com>
2021-12-21 11:59:17 +0200

    update logic

--- Steering behaviours/Models/Animal.cs
+++ Steering behaviours/Models/Animal.cs
@@ -7,7 +7,32 @@ namespace Steering_behaviours.Models
 {
     public abstract class Animal: Creature
     {
-        
+        private const float Epsilon = 0.05f;
+        public long Time { get; private set; }
+        private float velocityLimit = 3;
+        private float steeringForceLimit = 5;
+        public override void Update()
+        {
+            ApplyFriction();
+            ApplySteeringForce();
+            ApplyForces();
 
+        }
+
+        private void ApplyForces()
+        {
+            //todo: implemenation
+        }
+
+        private void ApplySteeringForce()
+        {
+            //todo: implemenation
+        }
+
+        private void ApplyFriction()
+        {
+            //todo: implemenation
+        }
     }
+
 }
--- Steering behaviours/Models/Creature.cs
+++ Steering behaviours/Models/Creature.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,18 @@ namespace Steering_behaviours.Models
     public abstract class Creature
     {
         protected Position Position { get; set; }
+        public int Health { get; private set; }
+        public double Weight { get; private set; }
+        public Color Color { get; set; }
+        public double MaxSpeed{get;private set;}
+
+        public bool Injure(int harm)
+        {
+            Health -= harm;
+            return Health > 0;
+        }
+
+        public abstract void Update();
+        
     }
 }
