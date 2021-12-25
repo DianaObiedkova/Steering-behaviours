8f62b8ff1bdc58f414f7e24467c5bfde0e6f21d9
Diana Obiedkova <missdee31102001@gmail.com>
2021-12-25 13:57:52 +0200

    mouse moving

--- Steering behaviours/wwwroot/js/site.js
+++ Steering behaviours/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let circle = document.getElementById('circle');
+const onMouseMove = (e) => {
+    circle.style.left = e.pageX - 400  + 'px';
+    circle.style.top = e.pageY - 120 + 'px';
+}
+document.addEventListener('mousemove', onMouseMove);
+
+//$(document).on("click mousemove", ".box", function (e) {
+//    var x = e.clientX;
+//    var y = e.clientY;
+//    var newposX = x - 60;
+//    var newposY = y - 60; $(".circle").css("transform", "translate3d(" + newposX + "px," + newposY + "px,0px)");
+//});
