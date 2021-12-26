151fb57ae17d7a329806dd0abbdbd5d5de7de8af
Diana Obiedkova <missdee31102001@gmail.com>
2021-12-26 20:00:05 +0200

    removed redundant code

--- Steering behaviours/wwwroot/js/ajax_requests.js
+++ Steering behaviours/wwwroot/js/ajax_requests.js
@@ -1,4 +1,58 @@
 ﻿
+function MoveHunter() {
+    $.ajax({
+        url: '/Game/MoveHunter',
+        type: 'POST',
+        data: {
+            "X": $(this).css("left"),
+            "Y": $(this).css("top")
+        },
+        success: function (response) {
+            alert(response);
+        }
+    });
+}
+
+function TakeShot() {
+    $.ajax({
+        url: '/Game/TakeShot',
+        type: 'POST',
+        data: {
+            "X": $(this).css("left"),
+            "Y": $(this).css("top")
+        },
+        success: function (response) {
+            alert(response);
+        }
+    });
+}
+
+function UpdateField() {
+    $.ajax({
+        url: '/Game/UpdateField',
+        type: 'POST',
+        //todo
+        data: {},
+        success: function (response) {
+            alert(response);
+        }
+    });
+}
+function GetPositions() {
+    $.ajax({
+        url: '/Game/GetMembersPositions',
+        type: 'GET',
+        success: function (response) {
+
+            let helper = document.getElementById("helper");
+            helper.insertAdjacentHTML('afterbegin', response);
+            //todo
+            alert(response);
+        }
+    });
+}
+
+//old
 function getPositions() {
     $.ajax({
         type: "GET",
