209aa6147be05cdb5e0cd83cd0b37526d71a0e5b
cyberg00se <zhuk.bohdana@gmail.com>
2021-12-27 22:57:46 +0200

    changed AvoidEdges

--- Steering behaviours/Models/Behaviours/AvoidEdges.cs
+++ Steering behaviours/Models/Behaviours/AvoidEdges.cs
@@ -15,7 +15,20 @@ namespace Steering_behaviours.Models.Behaviours
             var avoidDistance = 75;
             var distance = Position.Sub(an.Position);
 
-            return distance.Magnitude() > avoidDistance ? new Vector3() : distance.Normalize().Mult(-an.VelocityLimit);
+            if(an.Position.X>0 && an.Position.Y>0 && an.Position.X<750 && an.Position.Y<500)
+                return distance.Magnitude() > avoidDistance ? new Vector3() : distance.Normalize().Mult(-an.VelocityLimit);
+            else {
+                if(an.Position.X<0)
+                    an.UpdatePosition(new Vector3(an.Position.X-an.Position.X, an.Position.Y, 0));
+                if(an.Position.Y<0)
+                    an.UpdatePosition(new Vector3(an.Position.X, an.Position.Y-an.Position.Y, 0));
+                if(an.Position.X>750)
+                    an.UpdatePosition(new Vector3(an.Position.X-(an.Position.X-750), an.Position.Y, 0));
+                if(an.Position.Y>500)
+                    an.UpdatePosition(new Vector3(an.Position.X, an.Position.Y-(an.Position.Y-500), 0));
+                an.UpdatePosition(new Vector3( ));
+                return distance.Normalize().Mult(-an.VelocityLimit);
+            }
         }
     }
 }
